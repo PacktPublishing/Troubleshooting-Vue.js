15dfb73744c3a7b70e6bb348ba6a2673525e5e47
Christian Hur <ChristianHur@users.noreply.github.com>
2018-11-01 13:43:59 -0500

    Add files via upload

---
+++ Troubleshooting Vue.js Source/Troubleshooting Vue.js Source 3/Troubleshooting Vue.js Source 3.2/Troubleshooting Vue.js Source 3.2.Start/TroubleshootVue/src/components/About.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>About Troubleshooting Vue</h1>
+    <main role="main" class="container">
+      <p class="lead">
+        This course highlights some common problems faced by developers in different stages of their Vue application development
+        and shows you some simple and practical methods to try when troubleshooting, as well as how to solve more difficult
+        problems you may encounter.
+      </p>
+
+      <p class="lead">This course is for Vue.js developers who want to solve or avoid problems in their Vue application development and would
+        like to get some quick solutions to their common development issues. A good understanding of Web development (HTML,
+        CSS, JavaScript) and a basic knowledge of Vue.js framework, object-oriented programming (OOP) paradigm, and Model-View-Controller
+        (MVC) or Model-View-ViewModel (MVVM) architecture is expected.</p>
+    </main>
+
+  </div>
+</template>
+
+<script>
+
+import Vue from 'vue'
+
+  export default Vue.component("About", {
+    name: 'About'
+  });
+
+</script>
+
+
+<style scoped>
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+
+  .container {
+    text-align: left;  
+    }
+</style>
