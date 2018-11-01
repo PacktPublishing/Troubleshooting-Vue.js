99bb6c833260377695351231e1744729ba358a77
Christian Hur <ChristianHur@users.noreply.github.com>
2018-11-01 17:16:39 -0500

    Add files via upload

---
+++ Troubleshooting Vue.js Source/Troubleshooting Vue.js Source 3/Troubleshooting Vue.js Source 3.7/Troubleshooting Vue.js Source 3.7.Start/unit-test/test/unit/specs/HelloWorld.spec.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import { mount } from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld'
+
+describe('HelloWorld.vue', () => {
+
+  //Test 1 - setup correct
+  test ("Setup correct", ()=>{
+    expect(true).toBe(true);
+  })
+
+})
