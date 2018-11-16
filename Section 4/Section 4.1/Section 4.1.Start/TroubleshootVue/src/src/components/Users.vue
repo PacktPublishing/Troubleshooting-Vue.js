f31e270f4f4b419305122097e811c868f0ada319
ChristianHur <chrisvhur@gmail.com>
2018-11-16 15:07:41 -0600

    Move file to new directory

---
+++ Section 4/Section 4.1/Section 4.1.Start/TroubleshootVue/src/src/components/Users.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <h1 class="h3 mb-3 font-weight-normal">Show All Users</h1>
+    <main role="main" class="container">
+
+     
+        ID: <input v-model="id">
+        Name: <input v-model="name">
+        Username: <input v-model="username">
+        Email: <input v-model="email">
+        <button v-on:click="add()">Add</button>
+
+      
+
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(user,index) in users" :key="user.id">
+            <td>{{index}}: {{user.id}}</td>
+            <td>{{user.name}}</td>
+            <td>{{user.contact['username']}}</td>
+            <td>{{user['contact'].email}}
+              <button v-on:click="del(index)">Remove</button>
+
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Users",
+    data() {
+      return {
+        users: [
+          {
+            id: 1,
+            name: "Christian Hur",
+            contact: {
+              username: "christianhur",
+              email: "chur@troubleshootvue.com"
+            }
+          },
+          {id: 2,
+            name: "Kelly Love",
+            contact: {
+              username: "kellylove",
+              email: "klove@domain1.com"
+            }
+          },
+          {id: 3,
+            name: "Lucy Miller",
+            contact: {
+              username: "lucymiller",
+              email: "lmiller978@domain2.net"
+            }
+          },
+          {id: 4,
+            name: "Kevin Jones",
+            contact: {
+              username: "kevinjones",
+              email: "kjones450@domainz.com"
+            }
+          }
+        ],
+        id:"",
+        name:"",
+        username:"",
+        email:""
+      };
+    },
+    methods:{
+      add(){
+        this.users.push({
+            id: this.id,
+            name: this.name,
+            contact: {
+              username: this.username,
+              email: this.email
+            }
+          })
+      },
+      del(key){
+        this.$delete(this.users,key);
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+</style>
---
+++ Section 4/Section 4.4/Section 4.4.Start/TroubleshootVue/src/components/Counter.vue
@@ -0,0 +1,79 @@
+<template>
+   <div>
+       
+       <h1 v-bind:class="{active:isActive()}">{{title}}: {{counter}}</h1>
+
+       {{ isActive }}
+
+       <!--
+       <button @click="counter++">+</button>
+       <button @click="counter--">-</button>
+       -->
+    <my-message mid="1"></my-message>
+    <my-message mid="2"></my-message>
+    <my-message mid="3"></my-message>
+
+    <br>
+    <span v-for="item in items" :key="item.id">
+        {{item}}
+    </span>
+    <br>
+    Length: {{items.length}}
+    <br>
+    <input type="button" v-on:click="add()" value="Add">
+    <input type="button" v-on:click="sort()" value="Sort">
+    <br>
+
+</div>
+</template>
+
+<script>
+
+import Vue from 'vue'
+
+let msg = { message : "" };
+
+Vue.component("my-message",{
+    props: ["mid"],
+    template: `<div>Message #{{mid}} : <input v-model="d.message">
+                <p>{{ d.message }}</p></div>`,
+    data: function(){
+        return { d : { message : "" } }
+    }
+})
+
+
+var vm = new Vue({
+    el: "#app2",
+    template: "<div><h1>My new Vue</h1></div>"
+})
+
+
+export default {
+   data: function(){
+       return {
+           counter:0,
+           title:"Counter",
+           isActive: function(){ return true},
+           items : [20, 4, 11],
+           add: function(){
+               let i = this.items.length;
+                this.items[i] = i*2;
+               //this.items.push(i*2);
+               //this.items = this.items.concat(this.items);
+               console.table(this.items)
+           },
+           sort: function(){
+               this.items.sort();
+            console.table(this.items)
+           }
+        }
+   }
+}
+</script>
+
+<style scoped>
+.active { color: green; background-color: lightyellow;}
+ button { width:40px; padding: 3px;font-size: 20px;}
+ button:hover { background-color: #ccc;}
+</style>
