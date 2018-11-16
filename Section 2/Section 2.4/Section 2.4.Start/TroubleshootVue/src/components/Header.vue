0b0add2b822f0083a75710b551c90c266a9d6e1e
ChristianHur <chrisvhur@gmail.com>
2018-11-16 15:07:01 -0600

    Move file to new directory

---
+++ Section 2/Section 2.4/Section 2.4.Start/TroubleshootVue/src/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <!-- HEADER -->
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+            <router-link class="navbar-brand" to="/home">Troubleshooting Vue</router-link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- NAVIGATION -->
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <router-link class="nav-link" to="/home">Home
+                            <span class="sr-only">(current)</span>
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/about">About</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/users">All Users</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/user">Single User</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/login">Login</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/signup">Signup</router-link>
+                    </li>
+                </ul>
+                <Search/>
+            </div>
+        </nav>
+        <!-- END NAVIGATION -->
+    </div>
+    <!-- END HEADER -->
+</template>
+
+<script>
+
+    import Search from '@/components/Search'
+
+    export default {
+        name: "Header",
+        components : { Search }
+    };
+
+</script>
+
+<style>
+</style>
---
+++ Section 2/Section 2.6/Section 2.6.Start/TroubleshootVue/src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <!-- FOOTER -->
+      <footer class="footer">
+        <div class="container">
+          <p class="mb-1">&copy; 2018 Troubleshooting Vue.js</p>
+        </div>
+      </footer>
+    </div>
+    <!-- FOOTER -->  
+</template>
+
+<script>
+
+    export default {
+        name: "Footer"
+    };
+
+</script>
+
+<style>
+</style>
